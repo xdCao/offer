01dccde105eda0dbaf0bc4e33df50927c40a2d54
xdCao <705083979@qq.com>
2018-03-09 15:58:21 +0800

    刷题

---
+++ src/OfferPractice/PrintTree.java
@@ -0,0 +1,50 @@
+package OfferPractice;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * created by xdCao on 2018/3/9
+ */
+// todo 从上到下打印二叉树
+public class PrintTree {
+
+    class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+    private Queue<TreeNode> queue=new LinkedList<>();
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        if (root==null){
+            return null;
+        }
+        ArrayList<Integer> arrayList=new ArrayList<>();
+        TreeNode cur=root;
+        queue.add(cur);
+        while (!queue.isEmpty()){
+            printTree(arrayList,queue);
+        }
+        return arrayList;
+    }
+
+    private void printTree(ArrayList<Integer> arrayList, Queue<TreeNode> queue) {
+        TreeNode poll = queue.poll();
+        arrayList.add(poll.val);
+        if (poll.left!=null)
+            queue.add(poll.left);
+        if (poll.right!=null)
+            queue.add(poll.right);
+
+    }
+
+
+}
---
+++ src/OfferPractice/StackOrder.java
@@ -0,0 +1,32 @@
+package OfferPractice;
+
+import java.util.Stack;
+
+/**
+ * created by xdCao on 2018/3/9
+ */
+// todo 判断入栈出栈序列
+public class StackOrder {
+
+    Stack<Integer> stack=new Stack<>();
+
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        int index=0;
+        for (int i = 0; i < pushA.length; i++) {
+            stack.push(pushA[i]);
+            if (pushA[i]==popA[index]){
+                stack.pop();
+                index++;
+            }
+        }
+
+        for (int i = index; i < popA.length; i++) {
+            if (popA[i]!=stack.pop()){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
